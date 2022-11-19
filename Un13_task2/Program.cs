e043b4656d3e1385d9ae8975ae02089599a191a0
Setkliv <vovakosachev@mail.ru>
2022-11-19 10:58:47 +0300

    Добавьте файлы проекта.

---
+++ Un13_task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Un13_task2
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string book = File.ReadAllText(@"C:\Users\udav\Desktop\cdev_Text.txt"); //Считываем текст в строку
+            char[] chars = new char[] { ' ', '\n', '\r', '.', ',', '-', '-' };
+
+            string[] words = book.Split(chars, StringSplitOptions.RemoveEmptyEntries); //Разделяем на массив строк
+
+
+            var selectedWords = words.GroupBy(x => x).             //Группируем объекты
+                                OrderByDescending(x => x.Count()).  //Сортируем по убыванию по колличеству в группе
+                                Take(10).                           //берем первых 10 обектов 
+                                ToList();                           //Помещаем в коллекцию
+
+            foreach (var word in selectedWords)
+            {
+
+                Console.WriteLine($"Наиболее встречающееся слово в тексте (по убыванию) -  {word.Key}, колличество -  {word.Count()}");
+            }
+        }
+    }
+}
